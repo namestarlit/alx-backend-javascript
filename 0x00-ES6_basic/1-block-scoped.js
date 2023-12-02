c76970369e0123bcd4b2d1b608b5e5caa815d47c
Paul John <rainson.work@gmail.com>
2023-12-02 20:20:16 +0300

    update `1-block-scoped.js`

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,16 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
+    /**
+     * Disable eslint for the next two lines
+     * to suppress no-unused-vars error
+     * NOTE: single line disable: // eslint-disable-next-line
+     */
+
+    /* eslint-disable */
     const task = true;
     const task2 = false;
+    /* eslint-enable */
   }
 
   return [task, task2];
