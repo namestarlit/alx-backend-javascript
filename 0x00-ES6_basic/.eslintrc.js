4e19f565a8c16cfb5eebfefba611d8d5192a6ea8
Paul John <rainson.work@gmail.com>
2023-12-02 20:48:35 +0300

    update `1-block-scoped.js`

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -25,7 +25,6 @@ module.exports = {
       'LabeledStatement',
       'WithStatement',
     ],
-    'no-unused-vars': 'off'
   },
   overrides:[
     {
--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,17 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
+    /**
+     * Disable no-unused-vars eslint error
+     * for the next two lines as their are intentinally unused
+     * NOTE: use // eslint-disable-next-line [rule]
+     * to disble the next line below it
+     */
+
+    /* eslint-disable no-unused-vars */
     const task = true;
     const task2 = false;
+    /* eslint-enable no-unused-vars */
   }
 
   return [task, task2];
