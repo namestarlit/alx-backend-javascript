7433eebe2904feb79b3c08318a9507d09108c0e3
Paul John <rainson.work@gmail.com>
2023-12-04 10:51:59 +0300

    update `7-getBudgetObject.js` and `package.json`

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,10 +1,10 @@
+/**
+ * use object property value shorthand syntax
+ * if the value and the key are identical (have the same name)
+ * i.e. gdp: gdp -> gdp, income: income -> income, etc.
+ */
 export default function getBudgetObject(income, gdp, capita) {
   const budget = {
-    /**
-     * use object property value shorthand syntax
-     * if the value and the key are identical,
-     * i.e. gdp: gdp -> gdp, income: income -> income, etc.
-     */
     income,
     gdp,
     capita,
