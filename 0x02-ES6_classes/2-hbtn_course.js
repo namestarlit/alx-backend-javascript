cf1bc6e4ee363c38724ecb1f55c984d2b1594e7a
Paul John <rainson.work@gmail.com>
2023-12-16 20:02:06 +0300

    2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,13 +1,13 @@
 /* class representing HolbertonCourse class */
 export default class HolbertonCourse {
   /**
-   * create Holberton course
-   * @param {string} name - The name of the course
-   * @param {number} length - The duration of the course in years
-   * @param {Array.<string>} students - An array of students names
+   * Create a course.
+   * @param {string} name - Name of the course.
+   * @param {number} length - Length of time in years.
+   * @param {Array<string>} students - An array of students.
    */
   constructor(name, length, students) {
-    /* verify type of the inputs */
+    /* Verify type during object creation */
     if (typeof name !== 'string') throw new TypeError('Name must be a string');
     else if (typeof length !== 'number') throw new TypeError('Length must be a number');
     else if (!Array.isArray(students)) throw new TypeError('Students must be an array');
@@ -19,53 +19,34 @@ export default class HolbertonCourse {
   }
 
   /**
-   * getter method for name attribute
-   * @return {string} The course name
+   * Getter methods for each attribute.
+   * Setter methods for each attribute with type validation.
+   * @param {string|number|Array} value - The value to set.
    */
   get name() {
     return this._name;
   }
 
-  /**
-   * setter method for the name attribute
-   * @param {string} name - The course name
-   */
-  set name(name) {
-    if (typeof name !== 'string') throw new TypeError('Name must be a string');
-    this._name = name;
+  set name(value) {
+    if (typeof value !== 'string') throw new TypeError('Name must be a string');
+    this._name = value;
   }
 
-  /**
-   * getter method for length attribute
-   * @return {number} The length of the course in years
-   */
   get length() {
     return this._length;
   }
 
-  /**
-   * setter method for the length attribute
-   * @param {number} length - The length of the course in years
-   */
-  set length(length) {
-    if (typeof length !== 'number') throw new TypeError('Length must be a number');
-    this._length = length;
+  set length(value) {
+    if (typeof value !== 'number') throw new TypeError('Length must be a number');
+    this._length = value;
   }
 
-  /**
-   * getter method for students attribute
-   * @return {string} An array of students
-   */
   get students() {
     return this._students;
   }
 
-  /**
-   * setter method for the students attribute
-   * @param {string} students - The stundents array
-   */
-  set students(students) {
-    if (!(students instanceof Array)) throw new TypeError('Students must be an array');
-    this._length = students;
+  set students(value) {
+    if (!Array.isArray(value)) throw new TypeError('Students must be an array');
+    this._length = value;
   }
 }
