793e14e17c8f725360b3c144e8a67f76bc2c8617
Paul John <rainson.work@gmail.com>
2023-12-16 19:44:50 +0300

    update 2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -9,10 +9,8 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     /* verify type of the inputs */
     if (typeof name !== 'string') throw new TypeError('Name must be a string');
-
-    if (typeof length !== 'number') throw new TypeError('Length must be a number');
-
-    if (!Array.isArray(students)) throw new TypeError('Students must be an array');
+    else if (typeof length !== 'number') throw new TypeError('Length must be a number');
+    else if (!Array.isArray(students)) throw new TypeError('Students must be an array');
 
     /* eslint-disable no-underscore-dangle */
     this._name = name;
