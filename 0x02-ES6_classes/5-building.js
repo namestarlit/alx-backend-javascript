63b4c31e381f96e010fdb253f424aaaca501c710
Paul John <rainson.work@gmail.com>
2023-12-16 20:10:30 +0300

    5-building.js

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,33 @@
+/* class representing Building class */
+export default class Building {
+  /**
+   * Initializes Building
+   * @param {number} sqft
+   */
+  constructor(sqft) {
+    if (typeof sqft !== 'number') throw new TypeError('Square feet must be a number');
+
+    /* eslint-disable no-underscore-dangle */
+    this._sqft = sqft;
+    this.evacuationWarningMessage();
+  }
+
+  /**
+   * Getter method for sqft.
+   * @returns {number} - sqft
+   */
+  get sqft() {
+    return this._sqft;
+  }
+
+  /**
+   * Abstract method that must be implemented by subclasses.
+   * Throws an error if not overridden.
+   * @abstract
+   * @throws {Error} - If not overridden by subclass.
+   */
+  evacuationWarningMessage() {
+    /* eslint-disable-next-line class-methods-use-this */
+    if (this.constructor.name !== 'Building') throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
