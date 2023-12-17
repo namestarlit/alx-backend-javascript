704e68fdf95b1fd11066dd8219df5ec00ab1d979
Paul John <rainson.work@gmail.com>
2023-12-17 10:30:21 +0300

    0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+/**
+ * Gets a list of students.
+ * @returns {Array.<object>}
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
