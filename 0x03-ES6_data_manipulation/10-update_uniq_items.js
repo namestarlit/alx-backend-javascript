14e69fe3bb65f34b03b12ddb4d5f4ab7914a87f3
Paul John <rainson.work@gmail.com>
2023-12-17 12:27:06 +0300

    update 10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,10 +1,17 @@
 /**
- * Updates map values.
- * @param {Map} - map object
+ * Update the quantity to 100 for all items with initial quantity at 1 in the map.
+ * @param {Map} inputMap - The input map to be updated.
+ * @returns {Map} - The updated map.
+ * @throws {Error} - Throws an error if the input is not a map.
  */
-export default function updateUniqueItems(map) {
-  if (!(map instanceof Map)) throw new Error('Cannot process');
-  map.forEach((value, key) => {
-    if (value === 1) map.set(key, 100);
-  });
+export default function updateUniqueItems(inputMap) {
+  if (!(inputMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [item, quantity] of inputMap) {
+    if (quantity === 1) {
+      inputMap.set(item, 100);
+    }
+  }
 }
