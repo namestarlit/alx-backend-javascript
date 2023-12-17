ccd7a88e5d3324ce76af9205b09d8ce397aaf418
Paul John <rainson.work@gmail.com>
2023-12-17 12:12:46 +0300

    9-groceries_list.js

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+/**
+ * Create a map for groceries.
+ * @returns {Map} A map of groceries list
+ */
+export default function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasts', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
