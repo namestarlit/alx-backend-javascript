533fc11870006ac43ffb0b78ef3a7d87c14aec78
Paul John <rainson.work@gmail.com>
2023-12-17 11:21:02 +0300

    6-set.js

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,8 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - An array
+ * returns {Set} A set object
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
