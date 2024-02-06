b863250483e25f3bcf79be35cfb7edc69eb95c14
Paul John <rainson.work@gmail.com>
2024-02-06 16:02:18 +0300

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,5 +1,5 @@
-process.stdout.write('Welcome to Holberton, what is your name?\n');
-process.stdin.setEncoding('utf-8');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name) process.stdout.write(`Your name is: ${name}`);
