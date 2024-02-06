06f9399cbafe23e757e6855329baefaff83f5cf3
Paul John <rainson.work@gmail.com>
2024-02-06 17:20:23 +0300

    4-http.js

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
+
+app.on('request', (_, res) => {
+  const body = 'Hello Holberton School';
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', Buffer.byteLength(body));
+  res.end(body);
+});
+
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> htpp://${HOST}:${PORT}\n`);
+});
+
+module.exports = app;
